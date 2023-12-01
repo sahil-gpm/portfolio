abbf86bcc1d29457aa904faf370b53d0760bed6b
sahil <sahil@sahils-MacBook-Air.local>
2023-12-02 00:44:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Modules/Landingpage/Home";
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Fullstack from "./Modules/Projects/Fullstack";
+import Apibased from "./Modules/Projects/Apibased";
+import toast, { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 function App() {
+
+
+  useEffect(()=>{
+    toast.success("Welcome to my personal portfolio")
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              style: {
+                color: "#050a1f",
+                fontWeight : 500,
+              },
+              iconTheme: {
+                primary: '#aceb44',
+              },
+            },
+
+            error: {
+              style: {
+                color: "#050a1f",
+                fontWeight : 500,
+              },
+              iconTheme: {
+                primary: '#aceb44',
+              },
+            },
+          }}
+        />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" Component={Home}/>
+              <Route path="/sahil-full-stack-apps" Component={Fullstack}/>
+              <Route path="/sahil-newsfeed-api-based-app" Component={Apibased}/>
+          </Routes>
+      </BrowserRouter>
+   </div>
   );
 }
 
